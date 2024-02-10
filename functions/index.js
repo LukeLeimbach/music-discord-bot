682c1a9b20dca3cc7436cc64398771b261b74ad5
Xan Frank <slapmymemedaddy420@gmail.com>
2024-02-10 16:01:26 -0700

    hello world firebase function

--- functions/index.js
+++ functions/index.js
@@ -13,7 +13,7 @@ const logger = require("firebase-functions/logger");
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.helloWorld = onRequest((request, response) => {
+  logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
