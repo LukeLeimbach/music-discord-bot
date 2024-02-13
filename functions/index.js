e4bb97c278b4513454802fdbfb18ecd0494eeb10
Xan Frank <slapmymemedaddy420@gmail.com>
2024-02-13 15:31:54 -0700

    Start of bot, imported dotenv to hide BOT_TOKEN, updated Intents

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,7 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
+// Functions
 exports.helloWorld = onRequest((request, response) => {
   logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Wall Music's Server!");
---
+++ src/bot.js
@@ -0,0 +1,13 @@
+const {Client, IntentsBitField} = require('discord.js');
+require('dotenv').config()
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ],
+});
+
+client.login(process.env.BOT_TOKEN);
