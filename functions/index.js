5a0aff6860b924021330c9a4c4a2c28405ab9791
lukeleimbach <luke.leimbach@gmail.com>
2024-02-16 10:20:55 -0700

    bot

--- functions/index.js
+++
@@ -1,8 +0,0 @@
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
-
-// Functions
-exports.helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Wall Music's Server!");
-});
