dddd04f663dac6930b1559e7e1ed4e17f55c57a0
lukeleimbach <luke.leimbach@gmail.com>
2024-03-01 12:38:27 -0700

    Added Ionic Framework to index.js so that it renderes properly

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import { setupIonicReact } from '@ionic/react';
+
+// Ionic CSS imports
 import '@ionic/react/css/core.css';
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+// Optional themes (uncomment as needed)
+// import '@ionic/react/css/padding.css';
+// import '@ionic/react/css/float-elements.css';
+// import '@ionic/react/css/text-alignment.css';
+// import '@ionic/react/css/text-transformation.css';
+// import '@ionic/react/css/flex-utils.css';
+// import '@ionic/react/css/display.css';
+
+// Call setupIonicReact to configure Ionic
+setupIonicReact();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
