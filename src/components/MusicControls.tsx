c0157f9d9aebf4c5e8e350df9f89ea993b6811ec
lukeleimbach <luke.leimbach@gmail.com>
2024-03-08 16:27:33 -0700

    UI and theme changess

--- src/components/MusicControls.tsx
+++ src/components/MusicControls.tsx
@@ -7,11 +7,11 @@ function MusicControls() {
   const c = 'primary'
   return (
     <div className='control-container'>
-      <IonButton color={'dark'}><IonIcon icon={shuffleOutline} size='large' color={c} /></IonButton>
-      <IonButton color={'dark'}><IonIcon icon={playSkipBackCircleOutline} size='large' color={c} /></IonButton>
-      <IonButton color={'dark'}><IonIcon icon={playCircleOutline} size='large' color={c} /></IonButton>
-      <IonButton color={'dark'}><IonIcon icon={playSkipForwardCircleOutline} size='large' color={c} /></IonButton>
-      <IonButton color={'dark'}><IonIcon icon={infiniteOutline} size='large' color={c} /></IonButton>
+      <IonButton className={'dark-button'}><IonIcon icon={shuffleOutline} size='large' color={c} /></IonButton>
+      <IonButton className={'dark-button'}><IonIcon icon={playSkipBackCircleOutline} size='large' color={c} /></IonButton>
+      <IonButton className={'dark-button'}><IonIcon icon={playCircleOutline} size='large' color={c} /></IonButton>
+      <IonButton className={'dark-button'}><IonIcon icon={playSkipForwardCircleOutline} size='large' color={c} /></IonButton>
+      <IonButton className={'dark-button'}><IonIcon icon={infiniteOutline} size='large' color={c} /></IonButton>
     </div>
   );
 }
